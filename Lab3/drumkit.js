75d3c0e7727709a1dee94112e45bde0212bc4278
lukasz Zaraska <lukasz.zaraska@microsoft.wsei.edu.pl>
2022-10-22 12:30:22 +0200

    Panda 3

--- Lab3/drumkit.js
+++ Lab3/drumkit.js
@@ -1,5 +1,3 @@
-
-
 const START_RECORDING_KEY = ' '
 let RECORDING_TIME_START = 0
 let RECORDING_START = false
@@ -7,11 +5,13 @@ document.addEventListener('keypress', onKeyPress)
 
 const recorder_circle = document.querySelector('#circle')
 const list_track = document.querySelector('#list_track')
+const info = document.querySelector('#info')
+const playAll = document.querySelector('#PlayAll')
 let list_tracks = document.querySelectorAll('.play')
 
 let tracker_index = 1;
 let Track = []
-const Music = []
+let Music = []
 
 const Sound = {
     boom: 'boom',
@@ -20,48 +20,48 @@ const Sound = {
     tom: 'tom'
 };
 
-
+playAll.addEventListener('click',()=>{
+    for (let index = 0; index < tracker_index-1; index++) {
+        replay_track(index)
+        
+    }
+})
 
 document.addEventListener('click', function (e) {
     if (e.target.tagName === "BUTTON") {
-        console.log(e.target.parentNode.id)
-        replay_track(e.target.parentNode.id)
+        replay_track(e.target.parentNode.id - 1)
     }
 })
 
 function replay_track(id_track) {
-    // console.log(Music)
-    // Music.forEach(x=>{
-    //     console.log(x)
-    // })
+
+    let timer_start = Music[id_track][0][0]
+    let time = 0
+    let array = Music[id_track]
+
+    for (let index = 0; index < array.length; index++) {
+        time = array[index][0] - timer_start
+
+        setTimeout(() => KeyChoise(array[index][1]), time)
+    }
+
 }
-const record=(event)=> {
+
+const record = (event) => {
     if (event.key === START_RECORDING_KEY) {
 
         if (RECORDING_START === true) {
             RECORDING_START = false
-            recorder_circle.style.cssText += 'background-color:green';
-            console.log(Music)
-            Music.unshift(Track)
-            console.log(Music)
-            var track_div = document.createElement("div")
-            var button_start = document.createElement("button")
-            var content = document.createElement("div")
-            content.setAttribute("class", "content")
-            button_start.setAttribute("class", "play")
-            track_div.setAttribute("class", "track")
-            track_div.setAttribute("id", `${tracker_index}`)
-
-            track_div.appendChild(content)
-            track_div.appendChild(button_start)
-
-
-            content.innerHTML = `Nagranie numer ${tracker_index}`
-            list_track.appendChild(track_div)
-            tracker_index++
+           
+            if(tracker_index>4){
+                info.innerHTML = "Osiągnieto max ilości tracków"
+                return
+            }
+            Music.push(Track)
+            CreateHtml()
 
         } else {
-            Track.splice(0, Track.length)
+            Track = []
             RECORDING_TIME_START = Date.now()
             RECORDING_START = true
             recorder_circle.style.cssText += 'background-color:red';
@@ -75,40 +75,55 @@ function onKeyPress(event) {
     if (event.key === START_RECORDING_KEY) {
         return
     } else {
-        let Signature_time = 0
         const key = event.key
-
-        switch (key) {
-            case "q":
-                playSound(Sound.boom)
-                break;
-            case "w":
-                playSound(Sound.clap)
-                break;
-            case "e":
-                playSound(Sound.kick)
-                break;
-            case "r":
-                playSound(Sound.tom)
-                break;
-
-            default:
-                break;
-        }
-        if (RECORDING_START === true) {
-            Signature_time = Date.now()
-            Track.push(Signature_time)
-            Track.push(key)
-
-        }
+        KeyChoise(key)
     }
 
 
 }
 
-function playSound(sound) {
+function KeyChoise(key) {
+    switch (key) {
+        case "q":
+            playSound(Sound.boom, key)
+            break;
+        case "w":
+            playSound(Sound.clap, key)
+            break;
+        case "e":
+            playSound(Sound.kick, key)
+            break;
+        case "r":
+            playSound(Sound.tom, key)
+            break;
+
+        default:
+            break;
+    }
+}
+
+function playSound(sound, key) {
     const audioTag = document.querySelector('#' + sound)
     audioTag.currentTime = 0
     audioTag.play()
+    if (RECORDING_START === true) {
+        let Signature_time = Date.now()
+        Track.push([Signature_time, key])
+    }
+}
 
+function CreateHtml(){
+    recorder_circle.style.cssText += 'background-color:green';
+    var track_div = document.createElement("div")
+    var button_start = document.createElement("button")
+    var content = document.createElement("div")
+    content.setAttribute("class", "content")
+    button_start.setAttribute("class", "play")
+    track_div.setAttribute("class", "track")
+    track_div.setAttribute("id", `${tracker_index}`)
+    track_div.appendChild(content)
+    track_div.appendChild(button_start)
+    content.innerHTML = `Nagranie numer ${tracker_index}`
+    list_track.appendChild(track_div)
+    tracker_index++
 }
